19796874f8fedf49fb8469d45b46f0da9ccf7cf1
qinpmc <850823973@qq.com>
2018-06-18 21:28:13 +0800

    modify utils

--- ch7call+apply+bind+this/utils.js
+++ ch7call+apply+bind+this/utils.js
@@ -1,3 +1,4 @@
+/*
 var utils ={
     listToArray:function(likeAry){
         var arr=[];
@@ -20,4 +21,72 @@ var utils ={
         }
         return val;
     }
-}
+}*/
+var utils = (function(){
+    var flag = "getComputedStyle" in window; //检测是否是ie6，7，8
+
+    function listToArray(likeAry){
+        if(flag){
+            return [].slice.call(likeAry);
+        }
+        var arr=[];
+        for(var i=0;i<likeAry.length;i++){
+            ary[i] = likeAry[i];
+        }
+        return ary;
+    }
+
+    function getCss(ele,attr){
+        var res = null,reg = null;
+        if(flag){
+            res = window.getComputedStyle(ele,null)[attr];
+        }else{
+            if(attr =="opacity"){
+                res= ele.currentStyle["filter"];
+                reg = /^alpha\(opacity=(\d+(?:\.\d+)?)\)$/;i
+                res = reg.test(res)? reg.exec(res)[1]/100 :1;
+            }else{
+                res = ele.currentStyle[attr];
+            }
+        }
+        reg = /^(-?\d+(\.\d+)?)(px|pt|rem|em)?$/i;
+        return reg.test(res)? parseFloat(res):res;
+    }
+
+    function jsonParse(str){
+        return "JSON" in window ?JSON.parse(str): eval("("+str+")");
+    }
+
+    function getOffset(ele){
+        var top = ele.offsetTop,left= ele.offsetLeft,parent =ele.offsetParent ;
+        while(parent){
+            if(navigator.userAgent.indexOf("MSIE 8.0")==-1){ //ie8下已累加边框
+                //累加父级参照物的边框
+                left += parent.clientLeft;
+                top += parent.clientTop;
+            }
+
+            //累加父级参照物的偏移
+            left +=parent.offsetLeft;
+            top += parent.offsetTop;
+            parent = parent.offsetParent;
+        }
+        return {left:left,top:top};
+    }
+
+    function win(attr, value){
+        if(typeof value === "undefined"){
+            return document.documentElement[attr] || document.body[attr];
+        }
+        document.documentElement[attr] = value;
+        document.body[attr] = value;
+    }
+
+    return {
+        listToArray :listToArray,
+        getCss :getCss,
+        jsonParse:jsonParse,
+        getOffset:getOffset,
+        win:win
+    }
+})()
