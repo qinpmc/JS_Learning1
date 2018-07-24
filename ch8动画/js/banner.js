caa2e2f6cdaeb372a1cbc7a001ae8a0dbee09083
qinpmc <850823973@qq.com>
2018-07-24 21:37:46 +0800

    add imgs binding

--- ch8动画/js/banner.js
+++ ch8动画/js/banner.js
@@ -1,6 +1,7 @@
 (function(){
     var jsonData = null;
     var imgWrapper = utils.getElementByClass("wrap")[0];
+    var dots = utils.getElementByClass("dots")[0];
     // 1 绑定图片
 
     //1.1 请求图片数据
@@ -16,13 +17,30 @@
     }();
   // 1.2 绑定图片
     ~function(){
-        var str= null;
+        var str= "";
         if(jsonData){
             for(var i= 0,len=jsonData.length;i<len;i++){
-                str+="<img src='"+jsonData[i].img+"'>"
+                str+="<img src='"+jsonData[i].img+"'>";
+                
+
             }
         }
         imgWrapper.innerHTML = str;
+        utils.css(imgWrapper,"width",len*1024);
+
+        // 绑定焦点
+        str="";
+        for(i= 0;i<len;i++){
+            i==0? str+="<li class='active'></li>":str+="<li></li>";
+            dots.innerHTML = str;
+        }
+
     }()
+
+
+     //延迟加载
+
+
+
 
 })()
