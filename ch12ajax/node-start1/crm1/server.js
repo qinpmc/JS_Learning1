5149c6b273ea461f5b60d13affc0d122ac7bd3b7
qinpmc <850823973@qq.com>
2018-09-01 17:15:41 +0800

    add getList getCustom

---
+++ ch12ajax/node-start1/crm1/server.js
@@ -0,0 +1,117 @@
+var http = require("http"),
+    url= require("url"),
+    fs = require("fs");
+
+var httpServer = http.createServer(function(req,res){
+    var urlObj = url.parse(req.url,true),
+        pathName = urlObj.pathname,
+        query = urlObj.query;
+
+    var reg = /\.(HTML|CSS|JS)/i;
+    if(reg.test(pathName)){
+        var suffix = reg.exec(pathName)[1].toUpperCase();
+        var suffixMIME  = "text/html";
+        switch (suffix){
+            case "CSS":
+                suffixMIME = "text/css";
+                break;
+            case "JS":
+                suffixMIME = "text/javascript";
+                break;
+        }
+        try{
+            var conFile = fs.readFileSync("."+pathName,"utf-8");
+            res.writeHead(200,{"content-type":suffixMIME+";charset=utf-8"});
+            res.end(conFile);
+        }catch(e){
+            res.writeHead(400,{"content-type":suffixMIME+";charset=utf-8"});
+            res.end("request file is not find~~");
+        }
+        return;
+    }
+
+    //处理API
+
+    // 1 获取所有用户
+    var  result = null,
+        jsonPath = "json/customs.json";
+    var conFile = fs.readFileSync(jsonPath,"utf-8");
+    conFile.length==0? conFile ="[]":null; //防止json文件为空，获取的从File为空字符串，JSON.parse会报错。
+    conFile = JSON.parse(conFile);
+    result  = {
+        code :0,
+        msg:"没有信息",
+        data:null
+    };
+    if(pathName ==="/getList"){
+
+        if(conFile.length>0){
+            result  = {
+                code :1,
+                msg:"成功",
+                data:conFile
+            };
+        }
+        res.writeHead(200,{
+            "content-type":"application/json;charset=utf-8"
+        });
+        res.end(JSON.stringify(result));
+        return;
+    }
+
+    //2. 获取指定id用户信息
+    var customID =null,custom = null;
+    if(pathName ==="/getCustom"){
+        customID = query["id"];
+        console.log(conFile);
+        for(var i= 0,len=conFile.length;i<len;i++){
+            if(conFile[i].id == customID){
+                custom = conFile[i];
+                break;
+            }
+        }
+        if(conFile.length>0){
+            result  = {
+                code :1,
+                msg:"成功",
+                data:[custom]
+            };
+        }
+        res.writeHead(200,{
+            "content-type":"application/json;charset=utf-8"
+        });
+        res.end(JSON.stringify(result));
+        return;
+
+    };
+
+    //3. 删除指定id的用户
+    if(pathName ==="/deleteCustom"){
+        customID = query["id"];
+        console.log(conFile);
+        for(var i= 0,len=conFile.length;i<len;i++){
+            if(conFile[i].id == customID){
+                custom = conFile[i];
+                break;
+            }
+        }
+        if(conFile.length>0){
+            result  = {
+                code :1,
+                msg:"成功",
+                data:[custom]
+            };
+        }
+        res.writeHead(200,{
+            "content-type":"application/json;charset=utf-8"
+        });
+        res.end(JSON.stringify(result));
+        return;
+
+    };
+
+
+
+}).listen(8888,function(){
+    console.log("server listening on port 8888");
+})
